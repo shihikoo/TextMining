e481c76bfea103ac34ce8eec0250942e91991a94
shihiko <shihikoo@gmail.com>
2014-05-13 11:51:17 +0100

    test commit

--- pain_svm.r
+++ pain_svm.r
@@ -1,12 +1,32 @@
+# Script for cateragering papers
+#
+# This scipt is designed for finding "pain" related papers by reading the abstract
+# Current Data set is provided by Gillian and created by Gillian and Nickie
+# In the future, a download script may be added to directly download paper from the Endnote.
+#
+# Linear Logistic regression will be used first and if the accurancy is not ideal, SVM with different will be used.
+#
+# Created by Jing Liao
+# 12/05/2014
+#
 
-prepare <- function(){
-  setwd("M:/R/pain_svm")
+#function for set up the workpath, load the nesseary libraries
+prepare <- function(workpath="M:\Win7\Desktop\GitHub\", libraries = ""){
+  setwd(workpath)
+  if(libraries != "") {
+    for i in seq_along(libraries) {
+      library(libraries[i])
+    }
+  }
   prepared <- T
 }
 
-read_originalData <- function(file){
+#function for read the data from the file.
+readData <- function(datafile="data/Jing_painrefs_070514_short.txt"){
+# A manul preparition will be needed to change the original xlsx file to a tab seperated txt file, which may be done with Excel or similar program
+# The "short" have the same rows as the original file (33819), but only 6 columns (ID, Abstract, s1.included, s1.excluded, s2.included, s2.excluded)
   classes <- c("integer", "character", "logical","logical","logical","logical")
-  alldata<-read.delim("Jing_painrefs_070514_short.txt", stringsAsFactors=F,colClasses=classes)
+  alldata <- read.delim(datafile, stringsAsFactors=F,colClasses=classes)
 }
 
 cookdata <- function(alldata){
@@ -22,5 +42,5 @@ cookdata <- function(alldata){
 
 prepared <- F
 if(prepared != T) prepare()
-alldata <- read_originalData()
+alldata <- readData()
 data <- cookdata(alldata)
